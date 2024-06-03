eb1e2fa3cf70ea6d43feade74509750248763a65
mugecchi <migguirivera@gmail.com>
2024-06-03 16:18:07 +0800

    update

--- src/components/Purpose/Purpose.js
+++ src/components/Purpose/Purpose.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import "./Purposee.css";
+import "./Purpose.css";
 
 const Purpose = () => {
   const [note, setNote] = useState(
-    `The internship program is designed to serve as a valuable opportunity for students to gain firsthand experience in the corporate world, closely aligned with their chosen professional career paths. Through this program, students will be immersed in real-life business environments, allowing them to apply theoretical knowledge acquired in their academic studies to practical, everyday situations. This exposure not only helps bridge the gap between classroom learning and professional practice but also equips students with a deeper understanding of industry-specific practices, workplace dynamics, and professional expectations. By participating in this internship program, students will develop essential skills, gain critical insights into their future careers, and build a network of professional contacts that can support their career growth and aspirations. Ultimately, the program aims to prepare students for a successful transition from academic life to professional endeavours, ensuring they are well-prepared to meet the challenges and demands of their chosen fields.`
+    `The internship program is designed to serve as a valuable opportunity for students to gain firsthand experience in the corporate world, closely aligned with their chosen professional career paths. Through this program, students will be immersed in real-life business environments, allowing them to apply theoretical knowledge acquired in their academic studies to practical, everyday situations. This exposure not only helps bridge the gap between classroom learning and professional practice but also equips students with a deeper understanding of industry-specific practices, workplace dynamics, and professional expectations.`
   );
 
   const handleChange = (event) => {
@@ -13,12 +13,28 @@ const Purpose = () => {
   return (
     <div className="notepad">
       <div className="notepad-header" align="center">
-        PURPOSE OF INTERNSHP
+        PURPOSE OF INTERNSHIP
       </div>
       <div className="notepad-body">
         <textarea
           className="notepad-textarea"
           value={note}
+          onChange={handleChange}
+          align="justify"
+        />
+      </div>
+      <div className="notepad-body">
+        <textarea
+          className="notepad-textarea"
+          value={`By participating in this internship program, students will develop essential skills, gain critical insights into their future careers, and build a network of professional contacts that can support their career growth and aspirations.`}
+          onChange={handleChange}
+          align="justify"
+        />
+      </div>
+      <div className="notepad-body">
+        <textarea
+          className="notepad-textarea"
+          value={`Ultimately, the program aims to prepare students for a successful transition from academic life to professional endeavours, ensuring they are well-prepared to meet the challenges and demands of their chosen fields.`}
           onChange={handleChange}
           align="justify"
         />
