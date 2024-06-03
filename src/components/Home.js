9d18e21468e0dbfd2f08fb8f45ab5ce47b5b094f
mugecchi <migguirivera@gmail.com>
2024-06-03 10:01:21 +0800

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,14 @@ import {
   Box,
   Button,
   Container,
+  Grid,
   TextField,
   Typography,
   makeStyles,
   useMediaQuery,
 } from "@material-ui/core";
 import bg3 from "../images/Bg3.png";
+import bg2 from "../images/bg 2.png";
 import overlay from "../images/overlay.png";
 import WAVE from "../images/WAVE.svg";
 import InternData from "./Teams/Testimonials/InternData";
@@ -25,11 +27,11 @@ const useStyles = makeStyles((theme) => ({
     textAlign: "center",
     color: "white",
     position: "relative", // Changed from "absolute" to "relative"
-    height: "100vh", // Set the height to occupy the full viewport height
+    height: "130vh", // Set the height to occupy the full viewport height
     width: "100%",
     overflow: "hidden",
     "@media (max-width: 600px)": {
-      height: "50vh",
+      height: "40vh",
     },
   },
   bgImage: {
@@ -43,17 +45,27 @@ const useStyles = makeStyles((theme) => ({
       height: "50vh",
     },
   },
+  imageContainer: {
+    position: "absolute",
+    bottom: "15vw",
+    right: "5vw",
+    width: "60vw",
+    height: "30vw",
+    borderRadius: "5vw",
+    objectFit: "cover",
+    zIndex: -1,
+  },
   overlayImage: {
     position: "absolute",
     top: 0,
     left: "-2vw",
     width: "120vw",
     height: "100vh",
-    zIndex: -1,
+    zIndex: 1,
     opacity: 0,
   },
   textContainer: {
-    paddingTop: "20vh",
+    paddingTop: "10vh",
     paddingLeft: "2vw",
     paddingRight: "2vw",
   },
@@ -64,7 +76,7 @@ const useStyles = makeStyles((theme) => ({
     left: 0,
     width: "100vw",
     height: "100vh",
-    zIndex: -1,
+    zIndex: 1,
   },
   primaryPanel: {
     position: "relative",
@@ -90,67 +102,70 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: theme.spacing(0, 1, 1, 0),
     height: "7.2vh",
   },
+  text: {
+    fontSize: "2.5vw",
+    marginTop: "16vh",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "2.5vw",
+      marginTop: "6vh",
+    },
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "2.5vw",
+      marginTop: "5vh",
+    },
+  },
 }));
 
 const Home = () => {
-  const OutlineText = ({ children, fontSize, fontWeight, color }) => (
-    <Typography
-      style={{
-        fontSize: fontSize,
-        fontWeight: fontWeight,
-        lineHeight: 1,
-        color: "transparent",
-        WebkitTextStroke: `3px #000000`, // Outline color and width
-      }}
-    >
-      {children}
-    </Typography>
-  );
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <div className={classes.LandingPane}>
         <img alt="Background" src={bg3} className={classes.bgImage} />
-
+        <img alt="Interns" src={bg2} className={classes.imageContainer} />
         <div className={classes.textContainer}>
           <Box fontFamily="">
             <Typography align="center">
-              <OutlineText fontSize="4vw" fontWeight={600}>
-                CHART YOUR CAREER COURSE:
-                <br /> YOUR INTERNSHIP, YOUR FUTURE
-              </OutlineText>
-              <br />
-              <Typography
-                style={{ fontSize: "3vw", fontWeight: 600, color: "black" }}
-              >
-                <span style={{ color: "black", fontWeight: 600 }}>
-                  {" "}
-                  Learning.
-                </span>{" "}
-                <span
-                  style={{
-                    fontWeight: 600,
-                    WebkitTextStroke: `3px #000000`,
-                    color: "white",
-                  }}
+              <Grid xs="12">
+                <Typography
+                  style={{ fontSize: "3vw", fontWeight: 600, color: "black" }}
                 >
-                  Growing.
-                </span>{" "}
-                <span style={{ color: "#ff7704", fontWeight: 600 }}>
-                  Making an impact:
-                </span>
-                <br /> Explore the journeys of those who dared <br />
-                to bridge the gap between <br />
-                theory and reality.
-              </Typography>
+                  NAVIGATE YOUR{" "}
+                  <span style={{ color: "#ff7704" }}>CAREER ODYSSEY:</span>{" "}
+                  <br />
+                  FROM INTERNSHIP
+                  <span style={{ color: "#ff7704" }}> TO FUTURE SUCCESS</span>
+                  <br />
+                  <Grid item xs={4} style={{ textAlign: "left" }}>
+                    <Typography
+                      className={classes.text}
+                      style={{ fontWeight: 600 }}
+                    >
+                      Learning{" "}
+                      <span
+                        style={{
+                          WebkitTextStroke: "1px #000000",
+                          color: "transparent",
+                          fontSize: "4vw",
+                        }}
+                      >
+                        Growing.
+                      </span>{" "}
+                      <span style={{ color: "#ff7704" }}>
+                        {" "}
+                        Making an impact:
+                      </span>
+                      <br /> Explore the journeys of those who dared <br />
+                      to bridge the gap between <br />
+                      theory and reality.
+                    </Typography>
+                  </Grid>
+                </Typography>
+              </Grid>
             </Typography>
           </Box>
-          <img
-            alt="Overlay Background"
-            src={overlay}
-            className={classes.overlayImage}
-          />
+
           <img alt="Wave" src={WAVE} className={classes.WAVE} />
         </div>
       </div>
