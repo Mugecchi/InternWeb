749ecb2ff6f746c5114d53d6fe127ff048944a96
Mugecchi <162435412+Mugecchi@users.noreply.github.com>
2024-06-04 12:00:29 +0800

    Delete src directory

--- src/components/Gallery/utils/importImages.js
+++
@@ -1,9 +0,0 @@
-// src/utils/importImages.js
-
-export const importAllImages = (context) => {
-  let images = {};
-  context.keys().forEach((item) => {
-    images[item.replace("./", "")] = context(item);
-  });
-  return images;
-};
